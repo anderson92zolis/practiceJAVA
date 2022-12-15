d70087520f59077e68a5a8b708bbe66f83a8b28c
anderson92zolis <azolis92@hotmail.es>
2022-12-15 15:06:25 +0000

    Add PUBLIC to the class

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class ejercicio1 {
+public class ejercicio1 {
   public static void main(String[] args) {
     // 1) Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide realizar un algoritmo que intercambie los valores de ambas variables y muestre cuanto valen al final las dos variables (recuerda la asignación).
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
